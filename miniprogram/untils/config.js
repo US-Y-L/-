f67c0751418b2a5f144e3e1a274ca12ec41a4517
Wel <1195112836@qq.com>
2020-12-08 21:06:42 +0800

    新增管理员权限，只有管理员能修改菜品分类的增删改

--- miniprogram/untils/config.js
+++ miniprogram/untils/config.js
@@ -3,7 +3,7 @@ const tableName = {
   cate:"b_cate",
   menu:"b_menu"
 }
-//加入管理员id，只有管理员才能修改菜谱分类，假设自己是管理员，只有自己的openid才能进行菜单页面的增删改
+//加入管理员id，只有管理员才能修改菜谱分类，目前先假设自己就是管理员，只有自己的openid才能进行菜品分类的曾删改
 const admin_id = "ozpp-5e8ZcaKxnwtf3EhBHt_4VVA"
 export {
   tableName,
