daaeb88f36dcae362e0f56b265cbdcfd364f7a53
Wel <1195112836@qq.com>
2020-12-10 00:09:39 +0800

    将云环境更改为开发环境

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -10,7 +10,7 @@ App({
         //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
-        env: 'test-2gt4np9u582f5c45',
+        env: 'release-5gwqxl630da6dd9b', //准备上线时可以改成开发环境
         traceUser: true,
       })
     }
