753bd4446c1e1e04d959fdc132d7e65c6ae8b037
Wel <1195112836@qq.com>
2020-12-09 22:04:12 +0800

    增加b_collect表控制关注情况

--- miniprogram/untils/config.js
+++ miniprogram/untils/config.js
@@ -1,7 +1,8 @@
 const tableName = {
   user:"b_user",
   cate:"b_cate",
-  menu:"b_menu"
+  menu:"b_menu",
+  collect:"b_collect"
 }
 //加入管理员id，只有管理员才能修改菜谱分类，目前先假设自己就是管理员，只有自己的openid才能进行菜品分类的增删改
 const admin_id = "ozpp-5e8ZcaKxnwtf3EhBHt_4VVA"
