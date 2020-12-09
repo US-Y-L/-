7f2626065fa2635eb415ee1d40f6294b43ee226a
Wel <1195112836@qq.com>
2020-12-09 11:25:44 +0800

    添加列表页点击后交互效果

--- miniprogram/pages/list/list.js
+++ miniprogram/pages/list/list.js
@@ -1,3 +1,6 @@
+import Db from "../../untils/db"
+import {tableName,admin_id} from "../../untils/config"
+let {user,cate,menu} = tableName
 // pages/list/list.js
 Page({
 
@@ -5,61 +8,55 @@ Page({
    * 页面的初始数据
    */
   data: {
-    lists:[
-      {
-        src:"../../static/list/list01.jpg",
-        name:"土豆小番茄披萨",
-        userInfo:{
-          nickName:"林总小图",
-          pic:"../../static/list/users.png"
-        },
-        views:999,
-        follow:100
-      },
-      {
-        src:"../../static/list/list02.jpg",
-        name:"草莓巧克力三明治",
-        userInfo:{
-          nickName:"林总小图",
-          pic:"../../static/list/users.png"
-        },
-        views:88,
-        follow:200
-      },
-      {
-        src:"../../static/list/list03.jpg",
-        name:"法师意大利面",
-        userInfo:{
-          nickName:"林总小图",
-          pic:"../../static/list/users.png"
-        },
-        views:999,
-        follow:100
-      },
-      {
-        src:"../../static/list/list04.jpg",
-        name:"自制拉花",
-        userInfo:{
-          nickName:"林总小图",
-          pic:"../../static/list/users.png"
-        },
-        views:999,
-        follow:100
-      },
-      {
-        src:"../../static/list/list05.jpg",
-        name:"营养早餐",
-        userInfo:{
-          nickName:"林总小图",
-          pic:"../../static/list/users.png"
-        },
-        views:999,
-        follow:100
-      }
-    ]
+    lists:[],
+    id:"", //分类id
   },
   onLoad:function(option){
-    console.log(option)
+    this._setOptions(option)
+    //获取该分类下的菜谱
+    this._getMenu()
+  },
+  //获取该分类下的菜谱
+  _getMenu:async function(){
+    let res = await Db.findAll(menu,{cate_id:this.data.id,menu_status:0})
+    // res.map(async item=>{
+    //   let user1 = await Db.findAll(user,{_openid:item._openid})
+    // })
+    console.log(res.data)
+    if(res.data.length != 0){
+      //获取用户头像和昵称
+      let tasks = [];
+      res.data.forEach(async item=>{
+        let promise = Db.findAll(user,{_openid:item._openid})
+        tasks.push(promise)
+      })
+      let userArr = await Promise.all(tasks)
+      res.data.forEach((item,index)=>{
+        item.imgUrl = userArr[index].data[0].avatarUrl;
+        item.nickName = userArr[index].data[0].nickName;
+      })
+      console.log(res.data);
+      this.setData({
+        lists:res.data
+      })
+    }
+
+  },
+  //点击菜谱中的每一项，跳转到详情页
+  _goDetail(e){
+    let index = e.currentTarget.dataset.index
+    let {_id,menu_name} = this.data.lists[index]
+    wx.navigateTo({
+      url:`../detail/detail?id=${_id}&menuName=${menu_name}`
+    })
+  },
+  _setOptions:function(option){
+    this.setData({
+      id:option.id
+    })
+    //设置导航
+    wx.setNavigationBarTitle({
+      title:option.cateName
+    })
   }
- 
 })
